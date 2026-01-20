8739a217ac7d635fc31a13270ab01b4c733a6acd
chrysaliszt <chrysaliszt@gmail.com>
2026-01-19 19:02:38 -0800

    Add basic collisions

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -14,7 +14,11 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
         // wrap edges
         if(this.x <= 0 - this.width) {
-            this.x = game.config.width
+            this.reset()
         }
+    }
+
+    reset() {
+        this.x = game.config.width
     }
 }
